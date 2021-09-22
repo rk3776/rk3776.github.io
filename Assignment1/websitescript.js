375b0a091eb5e9ebc5399413e657e6e9b3b9234b
rk3776 <rk3776@nyu.edu>
2021-09-22 18:23:01 +0400

    uploading Assignment 1

---
+++ Assignment1/websitescript.js
@@ -0,0 +1,21 @@
+console.log("Welcome to the website for the Movie: Title of the Movie")
+
+function showDate()
+{
+	let button = document.getElementById("releasedate")
+	button.style.color = "black";
+}
+
+function Increase(x)
+{
+	x.style.height = "300px";
+	x.style.width = "300px";
+
+}
+
+function Normal(x)
+{
+	x.style.height = "180px";
+	x.style.width = "180px";
+
+}
